0c6fbe0bd54d1b431491e1f30954f721c926dd8e
= <=>
2024-07-20 10:17:11 +0300

    add

--- frontend/components/main/Nav.jsx
+++ frontend/components/main/Nav.jsx
@@ -1,57 +1,87 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Navbar.module.css';
-export default  function MainNavbar({info}) {
+import { useState } from 'react';
+
+export default function MainNavbar({ info }) {
+    const [userIsLogin, setUserIsLogin] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
 
     return (
-        <nav className={`navbar navbar-expand-lg  ${styles['bg-navbar']}`}>
+        <nav className={`navbar navbar-expand-lg ${styles['bg-navbar']}`}>
             <div className="container">
-
                 <Link href="/" className="navbar-brand">
-                        <Image 
-                            src={info.logo}
-                            className="d-inline-block align-text-top"
-                            width={30}
-                            height={24}
-                            alt={info.name}
-                        />
+                    <Image
+                        src={info.logo}
+                        className="d-inline-block align-text-top"
+                        width={30}
+                        height={24}
+                        alt={info.name}
+                    />
                 </Link>
-
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link href="/" legacyBehavior>
-                                <a className="nav-link active" aria-current="page">Home</a>
-                            </Link>
-                        </li>
-                    </ul>
-                    <hr />
-                    <ul className="nav d-flex justify-content-center d-lg-none">
-                        <Link href="/login" legacyBehavior>
-                            <button className="btn fw-bold mx-2">Login</button>
-                        </Link>
-                        <Link href="/register" legacyBehavior>
-                            <button className={`btn mx-2 fw-bold my-btn-secondary `}>Register</button>
-                        </Link>
-                    </ul>
-                </div>
-
+                {/* Desktop */}
                 <div className="ml-auto d-none d-lg-flex">
-                    <ul className="nav">
-                        <Link href="/login" legacyBehavior>
-                            <button className="btn fw-bold mx-2">Login</button>
-                        </Link>
+                    {!userIsLogin ? (
+                        <ul className="nav">
+                            <Link href="/login" legacyBehavior>
+                                <button className="btn fw-bold mx-2">Login</button>
+                            </Link>
+                            <Link href="/register" legacyBehavior>
+                                <button className="btn mx-2 fw-bold my-btn-secondary">Register</button>
+                            </Link>
+                        </ul>
+                    ) : (
                         <Link href="/register" legacyBehavior>
-                            <button className={`btn mx-2 fw-bold my-btn-secondary`}>Register</button>
+                            <button className="btn mx-2 btn-dark">Logout</button>
                         </Link>
-                    </ul>
+                    )}
                 </div>
+                {/* End Desktop */}
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                {/* Mobile */}
+                <button
+                    type="button"
+                    className="btn btn-light d-lg-none"
+                    onClick={handleShowModal}
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                
+
+                {showModal && (
+                    <div className="modal fade show d-block" id="mobileNav"  aria-labelledby="mobileNavLabel" aria-hidden="true">
+                        <div className="modal-dialog modal-xl">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="mobileNavLabel">Menu</h5>
+                                    <button type="button" className="btn-close" aria-label="Close" onClick={handleCloseModal}></button>
+                                </div>
+                                <div className="modal-body">
+                                    {!userIsLogin ? (
+                                        <ul className="nav flex-column">
+                                            <Link href="/login" legacyBehavior>
+                                                <button className="btn fw-bold my-2 btn-light">Login</button>
+                                            </Link>
+                                            <Link href="/register" legacyBehavior>
+                                                <button className="btn fw-bold my-2 my-btn-secondary">Register</button>
+                                            </Link>
+                                        </ul>
+                                    ) : (
+                                        <Link href="/register" legacyBehavior>
+                                            <button className="btn btn-dark my-2">Logout</button>
+                                        </Link>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+                {/* End Mobile */}
             </div>
         </nav>
-    )
+    );
 }
