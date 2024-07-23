249639e78aa7799028799654e579875b1f7e1228
= <=>
2024-07-23 19:21:25 +0300

    add

--- frontend/app/blog/page.jsx
+++ frontend/app/blog/page.jsx
@@ -15,18 +15,18 @@ export default async function Blog() {
     const posts = await fetchPosts();
 
   return (
-    <>
-    <div className="container mt-5 mb-5">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-            
-            {posts.map(post => (
-                <div className="col" key={post.slug}>
-                    <PostCard post={post} />
-                </div>
-            ))}
-            
+      <main>
+        <div className="container mt-5 mb-5">
+          <section className="app-section">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                {posts.map(post => (
+                    <div className="col" key={post.slug}>
+                        <PostCard post={post} />
+                    </div>
+                ))}
+            </div>
+          </section>
         </div>
-    </div>
-    </>
+      </main>
   )
 }
