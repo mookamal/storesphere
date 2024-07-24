0f0defd455d8374e76d89efc2e07d508fafde28d
= <=>
2024-07-24 11:10:33 +0300

    add

--- frontend/components/main/Footer.jsx
+++ frontend/components/main/Footer.jsx
@@ -1,13 +1,25 @@
 import Logo from "./Logo";
-
+import siteInfo from "../../data/site.json";
+import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="bg-dark p-5">
+    <footer className="bg-light">
       <section className="container">
-        <div className="min-w-100">
-          <Logo />
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 text-center">
+          <div className="col md-3"> <Logo /></div>
+          <div className="col mb-3">
+            <h5>{siteInfo.name}</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <Link href="/" className="nav-link p-0 text-body-secondary">Home</Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link href="/about" className="nav-link p-0 text-body-secondary">About</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </footer>
-  )
+  );
 }
