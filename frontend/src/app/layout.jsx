34237269e17ebb37b77b5966bcaf2e5092113a5f
= <=>
2024-07-24 18:41:55 +0300

    add

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,7 +1,13 @@
-import  "bootstrap/dist/css/bootstrap.min.css";
-import MainLayout from "./layouts/MainLayout";
+import  "bootstrap/dist/css/bootstrap.min.css";;
 import '../styles/globals.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import BootstrapJS from "../helper/BootstrapJS";
+
 export default function RootLayout({ children }) {
-    return <MainLayout children={children} />
-  }
+  return (
+    <html lang="en">
+      <head><BootstrapJS /></head>
+      <body>{children}</body>
+    </html>
+  )
+}
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,23 +1,28 @@
-import FeatureBanner from "../components/main/home/FeatureBanner"
-import StructureMetadata from '../helper/structureMetadata';
+import FeatureBanner from "../components/main/home/FeatureBanner";
+import StructureMetadata from "../helper/structureMetadata";
 import HomeSupport from "../components/main/home/HomeSupport";
 import siteInfo from "../data/site.json";
 import Hero from "../components/main/home/Hero";
-export  function generateMetadata({ params }) {
-  
-    return StructureMetadata({
-      title: siteInfo.name,
-      description: siteInfo.description,
-      image: siteInfo.logo,
-    });
-  }
+import Footer from "../components/main/Footer";
+import Navbar from "../components/main/Nav";
+export function generateMetadata({ params }) {
+  return StructureMetadata({
+    title: siteInfo.name,
+    description: siteInfo.description,
+    image: siteInfo.logo,
+  });
+}
 
-export default  function  Home() {
-    return (
-        <main>
-            <Hero />
-            <FeatureBanner />
-            <HomeSupport />
-        </main>
-    )
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <FeatureBanner />
+        <HomeSupport />
+      </main>
+      <Footer />
+    </>
+  );
 }
