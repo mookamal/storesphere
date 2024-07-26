8133aa89c398106b62842842d171d9f2cd6b7960
= <=>
2024-07-26 14:12:00 +0300

    add hero

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <>
       <MainNavbar />
-        
+      <Hero />
       <MainFooter />
     </>
   );
--- frontend/src/components/main/home/hero.jsx
+++ frontend/src/components/main/home/hero.jsx
@@ -1,19 +1,22 @@
 import siteInfo from "../../../data/site.json";
 import Link from 'next/link';
+import { FaArrowRight } from "react-icons/fa";
 export default function Hero() {
   return (
-    <div className="d-flex bg-color-white  justify-content-center align-items-center vh-100">
-      <div className="p-5">
-        <div className="container text-center">
-          <h1 className="display-4 font-weight-bold text-dark">
-            {siteInfo.name}
-          </h1>
-          <p className="mt-4 lead text-muted">
-            {siteInfo.description}
-          </p>
-          <Link href="/register" className="btn btn-primary">Start free trial</Link>
+      <section className="bg-center bg-no-repeat bg-[url('/assets/images/hero.jpg')] bg-gray-700 bg-blend-multiply">
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+          <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="http://accounts.nour.com/signup" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center  text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+              Get started
+              {/* <FaArrowRight /> */}
+            </a>
+            <a href="http://accounts.nour.com/login" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+              Login
+            </a>  
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
   );
 }
