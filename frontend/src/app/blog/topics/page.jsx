b4b438443653b0a3937e07ecc28e9721e37881b3
= <=>
2024-08-04 18:41:14 +0300

    add

--- frontend/src/app/blog/topics/page.jsx
+++ frontend/src/app/blog/topics/page.jsx
@@ -1,6 +1,6 @@
 import { fetchCategories } from '../../api/main/blog/category';
 import Link from 'next/link';
-import Image from 'next/image';
+import { Card } from "flowbite-react";
 import StructureMetadata from '../../../helper/structureMetadata';
 import siteInfo from "../../../data/site.json";
 
@@ -16,39 +16,24 @@ export default async function Topics() {
     const categories = await fetchCategories();
   return (
     <>
-    <section className="bg-white p-5 border-top">
-        <div className="container">
-            <h1 className="text-center">Blog Topics</h1>
+    <section className="bg-gray-100 dark:bg-black py-10">
+        <div className="container mx-auto">
+            <h1 className="text-center text-blue-800 dark:text-gray-50 font-bold text-4xl">Blog Topics</h1>
         </div>
     </section>
 
-    <div className="container p-5">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+    <div className="container mx-auto py-5">
+        <div className="grid grid-cols-2 lg:grid-cols-5 px-2 md:px-0 gap-2">
             {categories.map(category => (
-                <div key={category.slug} className="col">
-                    <div className="card bg-transparent h-100 border-0" style={{ position: 'relative', width: '100%', height: 'auto' }}>
-
-                        <Link href={`/blog/topics/${category.slug}`}>
-                        <div style={{ position: 'relative', width: '100%', paddingBottom: '56.25%' }}> 
-                            <Image
-                                src={category.image}
-                                alt={category.name}
-                                fill 
-                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                style={{ objectFit: 'cover' }}
-                            />
-                        </div>
-                        </Link>
-
-                        <div className="card-body">
-                        <Link href={`/blog/topics/${category.slug}`} className='text-decoration-none'>
-                            <h2 className='my-text-secondary h5'>{category.name}</h2>
-                        </Link>
-                        <p>{category.description}</p>
-                        </div>
-
-                    </div>
-                </div>
+                <Link key={category.slug} href={`/blog/topics/${category.slug}`}>
+                    <Card
+                    imgAlt={category.name}
+                    imgSrc={category.image}
+                    className='h-full shadow-sm'
+                    >
+                        <h3 className='text-xl font-bold hover:text-blue-600 text-gray-700 dark:text-gray-50'>{category.name}</h3>
+                    </Card>
+                </Link>
             ))}
         </div>
     </div>
