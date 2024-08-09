9ef27d4640a69bd2b4d3e4a919516bb7bf3de0bf
= <=>
2024-08-10 00:46:21 +0300

    add auth signup

---
+++ frontend/src/app/accounts/auth/signup/route.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+  try {
+    const { username, email, password1, password2 } = await request.json();
+
+    // Ensure the environment variable is defined
+    const backendUrl = process.env.NEXTAUTH_BACKEND_URL;
+    if (!backendUrl) {
+      throw new Error('Environment variable NEXTAUTH_BACKEND_URL is not defined');
+    }
+
+    // Send POST request
+    const response = await axios.post(
+      `${backendUrl}/register/`,
+      { username, email, password1, password2 }
+    );
+    
+    console.log('Response:', response.data);
+
+    // Return the response data
+    if (response.status === 201) {
+      return NextResponse.json({ success: response.data }, { status: 200 });
+    } else {
+      return NextResponse.json({ error: 'Registration failed!' }, { status: response.status });
+    }
+  } catch (error) {
+    console.error('Error in POST request:', error.message);
+    if (error.response && error.response.status === 400) {
+        const { data } = error.response;
+        return NextResponse.json({ error: data }, { status: 400 });
+    }
+  }
+}
