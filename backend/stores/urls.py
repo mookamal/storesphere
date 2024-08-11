02f77fba116123cbbe204e71e7294f33979f3ae8
= <=>
2024-08-12 00:04:37 +0300

    add

--- backend/stores/urls.py
+++ backend/stores/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import StoreListView
+from .views import StoreListView , StoreDetailView
 
 urlpatterns = [
     path('stores/', StoreListView.as_view(), name='store-list'),
+    path('stores/<str:domain>/', StoreDetailView.as_view(), name='store-detail'),
 ]
--- backend/stores/views.py
+++ backend/stores/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
-from rest_framework import authentication, permissions
+from rest_framework import authentication, permissions , status
 from rest_framework.response import Response
 from .models import Store
 from .serializer import StoreSerializer
+from django.shortcuts import get_object_or_404
 
 class StoreListView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
@@ -14,3 +15,22 @@ class StoreListView(APIView):
             return Response(serializer.data)
         else:
             return Response({'message': 'No stores found.'}, status=404)
+
+class StoreDetailView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    
+    def get(self, request, domain):
+        try:
+            store = get_object_or_404(Store , domain=domain)
+            serializer = StoreSerializer(store)
+            return Response(serializer.data)
+        except Store.DoesNotExist:
+            return Response({'message': 'Store not found.'}, status=404)
+    
+    def post(self, request):
+        # create store
+        serializer = StoreSerializer(data=request.data)
+        if serializer.is_valid():
+            store = serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
