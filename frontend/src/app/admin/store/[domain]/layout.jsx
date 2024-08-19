d35216f22ff5a38a1442381785145fa3014f60c6
= <=>
2024-08-19 06:03:00 +0300

    add

--- frontend/src/app/admin/store/[domain]/layout.jsx
+++ frontend/src/app/admin/store/[domain]/layout.jsx
@@ -1,10 +1,13 @@
 import AdminNavbar from "../../../../components/admin/AdminNavbar";
-
+import AdminSidebar from "../../../../components/admin/AdminSidebar";
 export default function AdminLayout({ children }) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
         <AdminNavbar />
-        <main>{children}</main>
-    </>
+        <div className="flex flex-1">
+        <AdminSidebar />
+        <main className="flex-1 p-6">{children}</main>
+        </div>
+    </div>
   )
 }
--- frontend/src/components/admin/AdminNavbar.jsx
+++ frontend/src/components/admin/AdminNavbar.jsx
@@ -8,31 +8,37 @@ import { DarkThemeToggle } from "flowbite-react";
 
 export default function AdminNavbar() {
   return (
-    <Navbar fluid rounded className="bg-slate-100 dark:bg-black">
-    <Navbar.Brand>
-      <img src={infoData.logo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-    </Navbar.Brand>
-    <div className="flex md:order-2 gap-2">
-      <DarkThemeToggle />
-      <Dropdown
-        arrowIcon={false}
-        inline
-        label={
-          <AvatarByLetter storeName="my Store" />
-        }
-      >
-        <Dropdown.Header>
-          <span className="block text-sm">Bonnie Green</span>
-          <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-        </Dropdown.Header>
-        <Dropdown.Item>Dashboard</Dropdown.Item>
-        <Dropdown.Item>Settings</Dropdown.Item>
-        <Dropdown.Item>Earnings</Dropdown.Item>
-        <Dropdown.Divider />
-        <Dropdown.Item>Sign out</Dropdown.Item>
-      </Dropdown>
-    </div>
-    <Search />
+    <Navbar fluid className="sticky top-0">
+      <div className="w-full p-1 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+          <Navbar.Brand>
+            <img src={infoData.logo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+          </Navbar.Brand>
+          </div>
+          <div className="flex md:order-2 gap-2">
+            <DarkThemeToggle />
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <AvatarByLetter storeName="my Store" />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">Bonnie Green</span>
+                <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+              </Dropdown.Header>
+              <Dropdown.Item>Dashboard</Dropdown.Item>
+              <Dropdown.Item>Settings</Dropdown.Item>
+              <Dropdown.Item>Earnings</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+          </div>
+          <Search />
+        </div>
+      </div>
   </Navbar>
   )
 }
