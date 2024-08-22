cd4d5a08859f75c5c5dfce759f45f09a0fc0f52e
= <=>
2024-08-23 00:45:43 +0300

    add

--- frontend/src/app/admin/store/[domain]/settings/general/page.jsx
+++ frontend/src/app/admin/store/[domain]/settings/general/page.jsx
@@ -1,5 +1,29 @@
+"use client";
+
+import { IoMdHome } from "react-icons/io";
+import { MdEditNote } from "react-icons/md";
+import { useState } from "react";
+import ProfileStoreModal from "../../../../../../components/admin/settings/general/ProfileStoreModal";
 export default function General() {
+  const [openProfileStoreModal, setOpenProfileStoreModal] = useState(false);
   return (
-    <div className="">generals</div>
+    <div className="lg:w-4/6 w-full">
+      {/* title */}
+      <h1 className="h1">General</h1>
+      {/* store details */}
+      <div className="card p-3 font-medium text-sm my-3">
+        <h2>Store details</h2>
+        <div className="border p-3 my-3 rounded-lg">
+          <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <IoMdHome size={24} className="mr-3 text-gray-500 dark:text-gray-50" />
+                <h2>My store</h2>
+              </div>
+              <button className="p-1 active-click" onClick={() => setOpenProfileStoreModal(true)}><MdEditNote size={24} className="text-gray-500 dark:text-gray-50" /></button>
+              <ProfileStoreModal openModal={openProfileStoreModal} setOpenModal={setOpenProfileStoreModal} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/app/admin/store/[domain]/settings/layout.jsx
+++ frontend/src/app/admin/store/[domain]/settings/layout.jsx
@@ -7,7 +7,7 @@ export default function layout({ children }) {
       <div className="hidden md:block xl:w-1/5 w-2/5 xl md:md:flex justify-center card p-2">
         <SettingsSidebar />
       </div>
-      <div className="p-4 flex justify-center card w-full md:w-3/5">
+      <div className="p-4 flex w-full md:w-3/5">
         {children}
       </div>
     </div>
