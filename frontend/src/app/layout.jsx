1185f8b7ee01bb8fa6bc65fd0e46b38508434e5c
= <=>
2024-08-24 03:32:09 +0300

    add

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,12 +1,9 @@
 import '../styles/globals.css';
-import SessionProvider from "../providers/AuthNextProvider";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className='dark:bg-slate-700'>
-        <SessionProvider>
           {children}
-        </SessionProvider>
       </body>
     </html>
   )
