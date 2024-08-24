a4419686dacf9ef6a54a966d6b69a2d7369330b1
= <=>
2024-08-24 03:29:50 +0300

    add

--- frontend/src/app/admin/layout.jsx
+++ frontend/src/app/admin/layout.jsx
@@ -4,19 +4,34 @@ import { useEffect } from 'react';
 import { fetchAccessToken } from "@/lib/utilities";
 
 export default function layout({ children }) {
+    const now = new Date();
     async function getToken() {
         try {
             const token = await fetchAccessToken();
-            localStorage.setItem('adminConfig', JSON.stringify({ token }));
+            const ttlSeconds = 86400000;
+            const expires = now.getTime() + ttlSeconds;
+            const adminConfig = {
+                token,
+                expires,
+            };
+            localStorage.setItem('adminConfig', JSON.stringify(adminConfig));
         } catch (err) {
             console.error(err);
             localStorage.removeItem('adminConfig');    
         }
     }
     useEffect(() => {
+        
         const savedConfig = localStorage.getItem('adminConfig');
         if (!savedConfig) {
             getToken();
+        } else {
+            const config = JSON.parse(savedConfig);
+            const { expires } = config;
+            if (now.getTime() > expires) {
+                localStorage.removeItem('adminConfig');
+                getToken();
+            }
         }
     }, []);
 
