a3e6e57a240d91e65c568fdf035fb78936b3bdd7
= <=>
2024-08-29 08:04:00 +0300

    add

--- backend/stores/schema.py
+++ backend/stores/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Store,StoreAddress
+from .models import Store,StoreAddress , StaffMember
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.core.exceptions import PermissionDenied
 
@@ -20,22 +20,28 @@ class AuthenticationMiddleware:
 class StoreAddressType(DjangoObjectType):
     class Meta:
         model = StoreAddress
+        fields = ('address1', 'address2', 'city', 'country_code_v2', 'company', 'phone', 'province_code', 'zip', )
 
 
 class StoreType(DjangoObjectType):
+    billing_address = graphene.Field(StoreAddressType)
     class Meta:
         model = Store
+    
+    def resolve_billing_address(self, info):
+        return self.billing_address if self.billing_address else None
 
 class Query(graphene.ObjectType):
     shop = graphene.Field(StoreType,default_domain=graphene.String(required=True))
 
-    def resolve_shop(self, info, domain):
+    def resolve_shop(self, info, default_domain):
         try:
             user = info.context.user
-            store = Store.objects.get(default_domain=domain)
-            if user.store_owner != store.owner:
+            store = Store.objects.get(default_domain=default_domain)
+            if StaffMember.objects.filter(user=user,store=store).exists():
+                return store
+            else:
                 raise PermissionDenied("You are not authorized to access this store.")
-            return store
         except Store.DoesNotExist:
             return None
         
--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -1,10 +1,11 @@
 import { gql  } from "@apollo/client";
 export const GET_SETTINGS_GENERAL = gql`
   query SettingsGeneral($domain: String!) {
-    shop(domain: $domain) {
+    shop(defaultDomain: $domain) {
       name
       email
       billingAddress {
+        countryCodeV2
         phone
       }
     }
