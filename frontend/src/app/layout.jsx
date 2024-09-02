8aabefd5994fca89d3e78017088d8108e0322d78
= <=>
2024-09-02 11:16:24 +0300

    add

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
+import 'react-toastify/ReactToastify.min.css';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
