603ca820a6ee908efc197fc4fb2b05d52e54e7e1
= <=>
2024-09-02 13:10:56 +0300

    add

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from stores.models import Store
+from django.template.defaultfilters import slugify
+from django_ckeditor_5.fields import CKEditor5Field
 # Create your models here.
+
+class Product(models.Model):
+    STATUS = (
+        ('ACTIVE', 'Active'),
+        ('DRAFT', 'Draft'),
+    )
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = CKEditor5Field('Description', config_name='extends')
+    handle = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS , default="DRAFT")
+    media_count = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.handle:
+            self.handle = slugify(self.title)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"Store {self.store.default_domain} | {self.title}"
