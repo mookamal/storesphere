785dc6e6fefb93593e9abcae089a4f4fe638e48c
= <=>
2024-09-04 09:57:55 +0300

    add

--- frontend/src/app/admin/store/[domain]/products/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/page.jsx
@@ -6,13 +6,14 @@ export default function Products() {
     <div className="w-full">
       <div className="card p-3 font-medium text-sm my-3">
         <h2>Filter</h2>
-        <div className="flex justify-around my-2 items-center border-b-2 p-2">
+        <div className="flex justify-around items-center p-2">
           <Dropdown label="Status" color="light">
             <Dropdown.Item value="all">All</Dropdown.Item>
             <Dropdown.Item value="ACTIVE">Active</Dropdown.Item>
             <Dropdown.Item value="DRAFT">Draft</Dropdown.Item>
           </Dropdown>
         </div>
+        <hr className="shadow" />
       </div>
     </div>
   )
