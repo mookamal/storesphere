23e72d44b604fda08c0d0854336d7e727559124e
= <=>
2024-09-04 09:56:37 +0300

    add

--- frontend/src/app/admin/store/[domain]/products/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/page.jsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { Dropdown } from "flowbite-react";
 export default function Products() {
   return (
-    <div>Products</div>
+    <div className="w-full">
+      <div className="card p-3 font-medium text-sm my-3">
+        <h2>Filter</h2>
+        <div className="flex justify-around my-2 items-center border-b-2 p-2">
+          <Dropdown label="Status" color="light">
+            <Dropdown.Item value="all">All</Dropdown.Item>
+            <Dropdown.Item value="ACTIVE">Active</Dropdown.Item>
+            <Dropdown.Item value="DRAFT">Draft</Dropdown.Item>
+          </Dropdown>
+        </div>
+      </div>
+    </div>
   )
 }
