c3adb205256e85131bbf7eb631a34cf2074e4e2b
= <=>
2024-09-04 10:11:11 +0300

    add

--- frontend/src/app/admin/store/[domain]/products/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Dropdown } from "flowbite-react";
+import { CiSearch } from "react-icons/ci";
+
 export default function Products() {
   return (
     <div className="w-full">
@@ -13,7 +15,15 @@ export default function Products() {
             <Dropdown.Item value="DRAFT">Draft</Dropdown.Item>
           </Dropdown>
         </div>
-        <hr className="my-2"/>
+        <hr className="my-2" />
+        <div className="flex justify-around items-center p-2">
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <CiSearch />
+              </div>
+              <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+            </div>
+        </div>
       </div>
     </div>
   )
