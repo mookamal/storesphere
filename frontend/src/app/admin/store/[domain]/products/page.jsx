bf6c63777e8fe73868998b27ca2e3175dbbe58bb
= <=>
2024-09-06 15:13:29 +0300

    add

--- frontend/src/app/admin/store/[domain]/products/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/page.jsx
@@ -2,7 +2,7 @@
 
 import { CiSearch } from "react-icons/ci";
 import Link from "next/link";
-import { Button, Checkbox, Table, Badge, Select ,Label } from "flowbite-react";
+import { Button, Checkbox, Table, Badge, Select, Label } from "flowbite-react";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import axios from 'axios';
@@ -10,7 +10,7 @@ import { PRODUCTS_ADMIN_PAGE } from "@/graphql/queries";
 import Lottie from 'lottie-react';
 import Error from "@/components/admin/Error";
 import animation from "@/assets/animation/loading.json";
-import { customThemeTable , customThemeSelect } from "@/lib/constants";
+import { customThemeTable, customThemeSelect } from "@/lib/constants";
 
 export default function Products({ params }) {
   const [error, setError] = useState(false);
@@ -32,7 +32,7 @@ export default function Products({ params }) {
     params.set(name, value);
     router.push(`${pathname}?${params.toString()}`);
   };
-  
+
   const getData = async () => {
     try {
       const response = await axios.post('/api/get-data', {
@@ -60,7 +60,7 @@ export default function Products({ params }) {
 
   useEffect(() => {
     getData();
-  }, [searchQuery,status]);
+  }, [searchQuery, status]);
 
   if (error) {
     return <Error />
@@ -72,68 +72,75 @@ export default function Products({ params }) {
 
   return (
     <div className="w-full">
-      <div className="card p-3 font-medium text-sm my-3">
-        <h2>Filter</h2>
-        <div className="flex justify-between items-center p-2">
-          <div className="flex flex-col gap-1 text-center">
-            <Label htmlFor="status">Status</Label>
+      {/* card */}
+      <div className="card my-3">
 
+        {/* header */}
+        <div className="card-header flex-col md:flex-row">
+          <h2>Filter</h2>
+          <div className="flex justify-between items-center p-2">
+            <div className="flex flex-col gap-1 text-center">
+              <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <CiSearch />
+                </div>
+                <input type="text" name="search" onChange={handleFilterChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* End header */}
+
+        {/* body */}
+        <div className="card-body">
+          <div className="flex justify-between items-center p-2">
             <Select name="status" theme={customThemeSelect} color="success" id="status" onChange={handleFilterChange} value={status}>
               <option value="all">All</option>
               <option value="ACTIVE">Active</option>
               <option value="DRAFT">Draft</option>
             </Select>
-
+            <Link href={`${pathname}/new`}>
+              <Button color="light" className="bg-yellow text-primary-text font-bold">
+                Add product
+              </Button>
+            </Link>
           </div>
-        </div>
-        <hr className="my-2" />
-        <div className="flex justify-between items-center p-2">
-          <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <CiSearch />
-            </div>
-            <input type="text" name="search" onChange={handleFilterChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
+          <div className="overflow-x-auto my-3">
+            <Table hoverable theme={customThemeTable}>
+
+              <Table.Head>
+                <Table.HeadCell>Product</Table.HeadCell>
+                <Table.HeadCell>Status</Table.HeadCell>
+              </Table.Head>
+
+              <Table.Body className="divide-y">
+                {products.map(({ node }) => (
+                  <Table.Row key={node.id}>
+
+                    <Table.Cell>
+                      <div className="flex justify-start gap-2 items-center">
+                        <Checkbox />
+                        <h2>{node.title}</h2>
+                      </div>
+                    </Table.Cell>
+
+                    <Table.Cell>
+                      <Badge color={node.status === 'ACTIVE' ? 'success' : 'warning'}>
+                        {node.status}
+                      </Badge>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+
+              </Table.Body>
+            </Table>
           </div>
-          <Link href={`${pathname}/new`}>
-            <Button color="light" className="bg-yellow text-primary-text font-bold">
-              Add product
-            </Button>
-          </Link>
-        </div>
-        <div className="overflow-x-auto my-3">
-          <Table hoverable theme={customThemeTable}>
-
-            <Table.Head>
-              <Table.HeadCell>Product</Table.HeadCell>
-              <Table.HeadCell>Status</Table.HeadCell>
-            </Table.Head>
-
-            <Table.Body className="divide-y">
-              {products.map(({ node }) => (
-                <Table.Row key={node.id}>
-
-                  <Table.Cell>
-                    <div className="flex justify-start gap-2 items-center">
-                      <Checkbox />
-                      <h2>{node.title}</h2>
-                    </div>
-                  </Table.Cell>
-
-                  <Table.Cell>
-                    <Badge color={node.status === 'ACTIVE'? 'success' : 'warning'}>
-                    {node.status}
-                    </Badge>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-
-            </Table.Body>
-          </Table>
-        </div>
-        <div className="flex justify-end">
-          {hasNextPage && <Button size="sm" color="light" className="bg-yellow text-primary-text font-bold" onClick={getData}>Load More</Button>}
+          <div className="card-footer justify-center md:justify-between flex-col md:flex-row gap-5 text-gray-600 text-2sm font-medium">
+            {hasNextPage && <Button size="sm" color="light" className="bg-yellow text-primary-text font-bold" onClick={getData}>Load More</Button>}
+          </div>
         </div>
       </div>
+      {/* End card */}
     </div>
   )
 }
