d4a9fbd32e96bfdc23766695b48c0968b5186076
= <=>
2024-09-08 11:36:27 +0300

    add

--- frontend/src/app/admin/store/[domain]/products/new/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/new/page.jsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { TextInput, Label } from "flowbite-react";
+
 export default function AddProduct() {
   return (
-    <div>AddProduct</div>
+    <div className="grid lg:grid-cols-3 gap-4">
+      <div className="lg:col-span-2">
+        <div className="card p-3">
+          <div>
+            <div className="mb-2">
+              <Label htmlFor="name" value="Product name" />
+            </div>
+            <TextInput id="name" sizing="sm" type="text" placeholder="Product 1" />
+          </div>
+        </div>
+      </div>
+      <div className="lg:col-span-1">
+        <div className="card">test2</div>
+      </div>
+    </div>
   )
 }
