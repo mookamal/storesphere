22f6d16fa524bd623fc87ef320375a306132fd8e
= <=>
2024-09-08 15:16:20 +0300

    add

--- frontend/src/app/admin/store/[domain]/products/new/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/new/page.jsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { TextInput, Label } from "flowbite-react";
+import dynamic from 'next/dynamic';
+
+const CustomEditor = dynamic( () => import( '@/components/custom-editor' ), { ssr: false } );
 
 export default function AddProduct() {
   return (
     <div className="grid lg:grid-cols-3 gap-4">
       <div className="lg:col-span-2">
         <div className="card p-3">
+
           <div>
             <div className="mb-2">
               <Label htmlFor="name" value="Product name" />
             </div>
             <TextInput id="name" sizing="sm" type="text" placeholder="Product 1" />
           </div>
+
+          <div>
+            <div className="mb-2">
+              <Label htmlFor="description" value="Product description" />
+            </div>
+            <CustomEditor id="description" name="description" />
+          </div>
+    
         </div>
       </div>
       <div className="lg:col-span-1">
---
+++ frontend/src/components/custom-editor.js
@@ -0,0 +1,69 @@
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  ClassicEditor,
+  Bold,
+  Essentials,
+  Italic,
+  Mention,
+  Paragraph,
+  Undo,
+  Heading,
+  Image,
+  ImageInsert,
+  
+} from "ckeditor5";
+import "ckeditor5/ckeditor5.css";
+import "../styles/custom-editor-styles.css";
+function CustomEditor() {
+  return (
+    <CKEditor
+      editor={ClassicEditor}
+      config={{
+        toolbar: {
+          items: ["undo", "redo", "|", "bold", "italic", "heading", "|","insertImage"],
+        },
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "Heading 3",
+              class: "ck-heading_heading3",
+            },
+          ],
+        },
+        plugins: [
+          Bold,
+          Essentials,
+          Italic,
+          Mention,
+          Paragraph,
+          Undo,
+          Heading,
+          Image,
+          ImageInsert,
+        ],
+        initialData: "<p>Hello from CKEditor 5 in React!</p>",
+      }}
+    />
+  );
+}
+
+export default CustomEditor;
