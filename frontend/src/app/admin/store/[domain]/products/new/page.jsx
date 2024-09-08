1763f8d72d100967f014ff3e5f4b600f05275494
= <=>
2024-09-08 19:17:04 +0300

    add

--- frontend/src/app/admin/store/[domain]/products/new/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/new/page.jsx
@@ -2,36 +2,54 @@
 
 import { TextInput, Label } from "flowbite-react";
 import dynamic from 'next/dynamic';
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const CustomEditor = dynamic( () => import( '@/components/custom-editor' ), { ssr: false } );
+const CustomEditor = dynamic(() => import('@/components/custom-editor'), { ssr: false });
 
 export default function AddProduct() {
-  const [description, setDescription] = useState("");
+  const { register, handleSubmit, setValue, watch } = useForm({
+    defaultValues: {
+      description: "",
+    },
+  });
+
+  const description = watch("description");
+
+  const onSubmit = (data) => {
+    console.log("Form Data: ", data);
+  };
+
+  const handleEditorChange = (content) => {
+    setValue("description", content);
+  };
+
   return (
-    <div className="grid lg:grid-cols-3 gap-4">
-      <div className="lg:col-span-2">
-        <div className="card p-3">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid lg:grid-cols-3 gap-4">
+        <div className="lg:col-span-2">
+          <div className="card p-3">
 
-          <div>
-            <div className="mb-2">
-              <Label htmlFor="name" value="Title" />
+            <div>
+              <div className="mb-2">
+                <Label htmlFor="title" value="Title" />
+              </div>
+              <TextInput id="title" sizing="sm" type="text" {...register("title")} />
             </div>
-            <TextInput id="name" sizing="sm" type="text" placeholder="Product 1" />
+
+            <div className="my-2">
+              <div className="mb-2">
+                <Label htmlFor="description" value="Description" />
+              </div>
+              {/* CustomEditor with description */}
+              <CustomEditor id="description" content={description} setContent={handleEditorChange} />
+            </div>
           </div>
 
-          <div className="my-2">
-            <div className="mb-2">
-              <Label htmlFor="description" value="Description"  />
-            </div>
-            <CustomEditor name="description" content={description} setContent={setDescription} />
-          </div>
         </div>
-
+        <div className="lg:col-span-1">
+          <div className="card">test2</div>
+        </div>
       </div>
-      <div className="lg:col-span-1">
-        <div className="card">test2</div>
-      </div>
-    </div>
+    </form>
   )
 }
