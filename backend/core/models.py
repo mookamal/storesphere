d91f7a1637833e56e8b305bebe559324eb064a4c
= <=>
2024-09-12 22:03:13 +0300

    add

--- backend/core/models.py
+++ backend/core/models.py
@@ -15,3 +15,10 @@ class Info(models.Model):
 
     def __str__(self):
         return self.name
+
+class SEO(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- backend/product/models.py
+++ backend/product/models.py
@@ -3,6 +3,7 @@ from stores.models import Store
 from django.template.defaultfilters import slugify
 from django_ckeditor_5.fields import CKEditor5Field
 from django.core.exceptions import ValidationError
+from core.models import SEO
 
 class Product(models.Model):
     STATUS = (
@@ -13,6 +14,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = CKEditor5Field('Description', config_name='extends')
     handle = models.CharField(max_length=255, null=True, blank=True)
+    seo = models.OneToOneField(SEO, on_delete=models.CASCADE, related_name="product", null=True, blank=True)
     status = models.CharField(max_length=10, choices=STATUS, default="DRAFT")
     media_count = models.IntegerField(default=0)
 
