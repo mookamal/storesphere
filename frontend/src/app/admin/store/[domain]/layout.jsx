a0c5d578be48736f3ccb661dc2bd829aedcb9818
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-10-31 09:13:23 +0300

    add

--- frontend/src/app/admin/store/[domain]/layout.jsx
+++ frontend/src/app/admin/store/[domain]/layout.jsx
@@ -3,18 +3,18 @@ import AdminSidebar from "@/components/admin/AdminSidebar";
 import "./globals.css";
 
 export const metadata = {
-  title: 'Store Management',
-  description: 'Store Management',
-}
+  title: "Store Management",
+  description: "Store Management",
+};
 
 export default function AdminLayout({ children }) {
-  
-
   return (
     <>
       <AdminNavbar />
       <AdminSidebar />
-      <main className="p-4 sm:ml-64 mt-16 h-screen bg-screen-primary dark:bg-coal-500">{children}</main>
+      <main className="p-4 sm:ml-64 mt-16 min-h-screen bg-screen-primary dark:bg-coal-500">
+        {children}
+      </main>
     </>
-  )
+  );
 }
