63b4f90908ab0125b3d1388716dfdf3ec224c680
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-11-03 08:54:44 +0200

    add

--- frontend/src/app/admin/store/[domain]/settings/general/page.jsx
+++ frontend/src/app/admin/store/[domain]/settings/general/page.jsx
@@ -56,7 +56,7 @@ export default function General({ params }) {
   }
 
   return (
-    <div className="grid md:grid-cols-2  gap-3 my-10">
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 my-10">
       {/* Store details */}
       <Card>
         <CardHeader>
