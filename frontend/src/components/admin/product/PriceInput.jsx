09824fcac7c975893d927862f961a67ec505f4de
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-11-05 15:55:29 +0200

    add

--- frontend/src/components/admin/product/PriceInput.jsx
+++ frontend/src/components/admin/product/PriceInput.jsx
@@ -1,49 +1,55 @@
 "use client";
-import { TextInput, Label } from "flowbite-react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function PriceInput({ register, currencyCode, price, compare }) {
   const showWarning = price > 0 && compare > 0 && price >= compare;
 
   return (
-    <div className="card p-3 flex flex-col h-full">
-      <h2>Pricing</h2>
-      <div className="flex justify-items-start my-5 gap-2">
-        <div>
-          <div className="mb-2">
-            <Label htmlFor="price" value="Price" />
+    <Card className="bg-gray-100 dark:bg-slate-900 shadow-md border-1">
+      <CardHeader>
+        <h2>Pricing</h2>
+      </CardHeader>
+      <CardContent>
+        <div className="flex justify-items-start gap-2">
+          <div>
+            <div className="mb-2">
+              <Label htmlFor="price">Price</Label>
+            </div>
+            <Input
+              id="price"
+              size="sm"
+              addon={currencyCode}
+              type="number"
+              placeholder="0.00"
+              {...register("price")}
+            />
           </div>
-          <TextInput
-            id="price"
-            sizing="sm"
-            addon={currencyCode}
-            type="number"
-            placeholder="0.00"
-            {...register("price")}
-          />
-        </div>
-        <div>
-          <div className="mb-2">
-            <Label htmlFor="compare" value="Compare-at price" />
+          <div>
+            <div className="mb-2">
+              <Label htmlFor="compare">Compare-at price</Label>
+            </div>
+            <Input
+              id="compare"
+              size="sm"
+              addon={currencyCode}
+              type="number"
+              placeholder="0.00"
+              {...register("compare")}
+            />
           </div>
-          <TextInput
-            id="compare"
-            sizing="sm"
-            addon={currencyCode}
-            type="number"
-            placeholder="0.00"
-            {...register("compare")}
-          />
         </div>
-      </div>
-      {showWarning && (
-        <p className="text-red-500 my-2">
-          Warning: Price should be less than the compare-at price.
+        {showWarning && (
+          <p className="text-red-500 my-2">
+            Warning: Price should be less than the compare-at price.
+          </p>
+        )}
+        <p className="my-2">
+          Please enter the compare price. This price represents the original or
+          previous price of the product before any discount.
         </p>
-      )}
-      <p>
-        Please enter the compare price. This price represents the original or
-        previous price of the product before any discount.
-      </p>
-    </div>
+      </CardContent>
+    </Card>
   );
 }
