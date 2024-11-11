da1d75e3280765d78f3cae8d1ce9abd9fc2e1f20
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-11-11 11:06:27 +0200

    add

--- frontend/src/components/admin/product/VariantInputs.jsx
+++ frontend/src/components/admin/product/VariantInputs.jsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { IoAddCircle } from "react-icons/io5";
 import { useFieldArray } from "react-hook-form";
-import OptionValues from "@/components/admin/product/OptionValues";
+import OptionValues from "@/components/admin/product/option/OptionValues";
 import VariantsTable from "./VariantsTable";
 import { Button } from "@/components/ui/button";
 export default function VariantInputs({
