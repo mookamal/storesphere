570bf1ca78a0822476841a60fca92f3f30516a24
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-11-15 13:24:07 +0200

    Create variants tables and set variants in this tables.

--- frontend/src/components/admin/product/variant/VariantsTable.jsx
+++ frontend/src/components/admin/product/variant/VariantsTable.jsx
@@ -1,14 +1,24 @@
 "use client";
 import axios from "axios";
-import { toast } from "react-toastify";
 import { useParams } from "next/navigation";
 import { ADMIN_PRODUCT_DETAILS_VARIANTS } from "@/graphql/queries";
 import { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 export default function VariantsTable() {
   const productId = useParams().id;
   const [variants, setVariants] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getVariantsByProductID = async () => {
+    setLoading(true);
     const variables = {
       productId: productId,
       first: 2,
@@ -19,14 +29,36 @@ export default function VariantsTable() {
         query: ADMIN_PRODUCT_DETAILS_VARIANTS,
         variables: variables,
       });
-      console.log("response", response);
+      if (response.statusText === "OK") {
+        setVariants(response.data.productDetailsVariants.edges);
+      }
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     getVariantsByProductID();
   }, []);
-  return <div>VariantsTable</div>;
+
+  if (loading) return <div>Loading...</div>;
+  return (
+    <Table>
+      <TableCaption>Variants</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Price</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {variants.map(({ node }) => (
+          <TableRow key={node.id}>
+            <TableCell>{node.price}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
