ddbbe7c5a9ccb383f9cf1cc4f051ccdb30c43bd8
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-09 07:59:37 +0200

    Implementation types to MailingAddress and Customer Models

---
+++ backend/customer/schema/types.py
@@ -0,0 +1,25 @@
+from ..models import MailingAddress, Customer
+import graphene
+from graphene_django import DjangoObjectType
+from django_countries.graphql.types import Country
+
+
+class MailingAddressType(DjangoObjectType):
+    company = graphene.Field(Country)
+
+    class Meta:
+        model = MailingAddress
+        fields = ['address1', 'address2', 'city', 'country',
+                  'company', 'phone', 'province_code', 'zip',]
+
+
+class CustomerNode(DjangoObjectType):
+    customer_id = graphene.Int()
+
+    class Meta:
+        model = Customer
+        interfaces = (graphene.relay.Node,)
+        exclude = ["store",]
+
+    def resolve_customer_id(self, info):
+        return self.id
