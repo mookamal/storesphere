09f6743fa656e82edf85766058326e445c32ddfc
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-10 13:34:51 +0200

    add

--- frontend/src/app/admin/store/[domain]/customers/new/page.jsx
+++ frontend/src/app/admin/store/[domain]/customers/new/page.jsx
@@ -1,16 +1,37 @@
 "use client";
 
 import CustomerOverview from "@/components/admin/customer/CustomerOverview";
-
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { IoReload } from "react-icons/io5";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 export default function CreateCustomer() {
+  const { register, handleSubmit, watch, setValue } = useForm();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="p-5">
         <h1 className="h1">New customer</h1>
         <div className="flex flex-col items-center my-5 gap-3">
           <CustomerOverview />
         </div>
       </div>
+      <Button
+        size="lg"
+        type="submit"
+        className="fixed bottom-5 right-5 rounded-full shadow-md"
+        disabled={loading}
+      >
+        {loading && <IoReload className="mr-2 h-4 w-4 animate-spin" />}
+        Add
+      </Button>
     </form>
   );
 }
