55303166aff9bda086d594e121df1d39d7c6fda3
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-14 15:33:22 +0200

    Implementation create customer nutation

--- backend/customer/schema/mutations.py
+++ backend/customer/schema/mutations.py
@@ -14,27 +14,35 @@ class CreateCustomer(graphene.relay.ClientIDMutation):
         default_domain = graphene.String(required=True)
 
     @classmethod
-    def mutate(cls, root, info, customer_inputs, default_domain):
+    def mutate_and_get_payload(cls, root, info, **input):
+        default_domain = input.get('default_domain')
+        customer_inputs = input.get('customer_inputs')
         user = info.context.user
         try:
             store = Store.objects.get(default_domain=default_domain)
             if not StaffMember.objects.filter(user=user, store=store).exists():
-                raise GraphQLError("You are not authorized to access this store.",              extensions={
-                    "code": "PERMISSION_DENIED",
-                    "status": 403
-                })
+                raise GraphQLError("You are not authorized to access this store.",
+                                   extensions={
+                                       "code": "PERMISSION_DENIED",
+                                       "status": 403
+                                   })
         except Store.DoesNotExist:
             raise GraphQLError("Store not found.",
                                extensions={
                                    "code": "NOT_FOUND",
                                    "status": 404
                                })
-        address = customer_inputs.address
-        del customer_inputs.address
-        default_address = MailingAddress.objects.create(**address)
-        customer_inputs.default_address = default_address
-        customer = Customer(**customer_inputs.dict())
+        if customer_inputs.default_address:
+            address = customer_inputs.default_address
+            customer_inputs.pop("default_address")
+            default_address = MailingAddress.objects.create(**address)
+        else:
+            default_address = None
+        customer = Customer(store=store,
+                            default_address=default_address, **customer_inputs,)
         customer.save()
+
+        return CreateCustomer(customer=customer)
 
 
 class Mutation(graphene.ObjectType):
