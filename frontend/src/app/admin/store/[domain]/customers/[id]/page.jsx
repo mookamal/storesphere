cc204a21d6721a5f2564a045729fe534328906fa
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-17 14:43:39 +0200

    add

--- frontend/src/app/admin/store/[domain]/customers/[id]/page.jsx
+++ frontend/src/app/admin/store/[domain]/customers/[id]/page.jsx
@@ -5,8 +5,9 @@ import { useState } from "react";
 
 export default function UpdateCustomer() {
   const customerId = useParams().id;
-  const domain = useParams().domain;
   const [loading, setLoading] = useState(false);
+  const [hasChanges, setHasChanges] = useState(false);
+  const [customer, setCustomer] = useState(null);
 
   return <div>{customerId}</div>;
 }
