86f5e18b52c605f41a3017c5f07a2032a5eb9adf
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-20 20:30:56 +0200

    add

--- backend/order/models.py
+++ backend/order/models.py
@@ -11,32 +11,32 @@ from . import (
 # Create your models here.
 
 
-class Order(ModelWithExternalReference):
-    store = models.ForeignKey('stores.Store', blank=True, null=True,
-                              related_name="orders", on_delete=models.SET_NULL)
-    id = models.UUIDField(primary_key=True, editable=False,
-                          unique=True, default=uuid4)
-    created_at = models.DateTimeField(default=now, editable=False)
-    updated_at = models.DateTimeField(
-        auto_now=True, editable=False, db_index=True)
-    status = models.CharField(
-        max_length=32, default=OrderStatus.OPEN, choices=OrderStatus.CHOICES
-    )
-    payment_status = models.CharField(
-        max_length=32, default=PaymentStatus.PENDING, choices=PaymentStatus.CHOICES
-    )
-    fulfillment_status = models.CharField(
-        max_length=32, default=FulfillmentStatus.UNFULFILLED, choices=FulfillmentStatus.CHOICES
-    )
-    return_status = models.CharField(
-        max_length=32, blank=True, null=True, choices=ReturnStatus.CHOICES
-    )
-    customer = models.ForeignKey(
-        'customer.Customer', blank=True, null=True, related_name="orders", on_delete=models.SET_NULL)
-    billing_address = models.ForeignKey(
-        "customer.Address",
-        related_name="+",
-        editable=False,
-        null=True,
-        on_delete=models.SET_NULL,
-    )
+# class Order(ModelWithExternalReference):
+#     store = models.ForeignKey('stores.Store', blank=True, null=True,
+#                               related_name="orders", on_delete=models.SET_NULL)
+#     id = models.UUIDField(primary_key=True, editable=False,
+#                           unique=True, default=uuid4)
+#     created_at = models.DateTimeField(default=now, editable=False)
+#     updated_at = models.DateTimeField(
+#         auto_now=True, editable=False, db_index=True)
+#     status = models.CharField(
+#         max_length=32, default=OrderStatus.OPEN, choices=OrderStatus.CHOICES
+#     )
+#     payment_status = models.CharField(
+#         max_length=32, default=PaymentStatus.PENDING, choices=PaymentStatus.CHOICES
+#     )
+#     fulfillment_status = models.CharField(
+#         max_length=32, default=FulfillmentStatus.UNFULFILLED, choices=FulfillmentStatus.CHOICES
+#     )
+#     return_status = models.CharField(
+#         max_length=32, blank=True, null=True, choices=ReturnStatus.CHOICES
+#     )
+#     customer = models.ForeignKey(
+#         'customer.Customer', blank=True, null=True, related_name="orders", on_delete=models.SET_NULL)
+#     billing_address = models.ForeignKey(
+#         "customer.Address",
+#         related_name="+",
+#         editable=False,
+#         null=True,
+#         on_delete=models.SET_NULL,
+#     )
