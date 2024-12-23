286f789b68a7dd1b8af7d0ba9e783c7ec23a7772
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-23 23:16:22 +0200

    add

--- frontend/src/app/admin/store/[domain]/products/[id]/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/[id]/page.jsx
@@ -302,7 +302,7 @@ export default function UpdateProduct() {
     setLoading(true);
     const productData = {
       title: data.title,
-      description: data.description,
+      description: JSON.stringify(data.description),
       status: data.status,
       handle: data.handle,
       collectionIds: selectedCollections.map((c) => c.collectionId),
