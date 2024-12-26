db252770ace1afae67c6bf503cf679548fc0a92e
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-26 17:39:47 +0200

    add

--- backend/stores/models.py
+++ backend/stores/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from accounts.models import User
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
-from .utility import generate_unique_subdomain
+from .utils import generate_unique_subdomain
 # Create your models here.
 
+
 class StoreAddress(models.Model):
-    store = models.OneToOneField('Store',on_delete=models.CASCADE,related_name="billing_address")
+    store = models.OneToOneField(
+        'Store', on_delete=models.CASCADE, related_name="billing_address")
     address1 = models.CharField(max_length=255, null=True, blank=True)
     address2 = models.CharField(max_length=255, null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
     country = CountryField(blank=True, null=True)
-    company = models.CharField(max_length=255,blank=True, null=True)
+    company = models.CharField(max_length=255, blank=True, null=True)
     phone = PhoneNumberField(blank=True, null=True)
     province_code = models.CharField(max_length=10, null=True, blank=True)
     zip = models.CharField(max_length=10, null=True, blank=True)
@@ -19,43 +21,53 @@ class StoreAddress(models.Model):
     def __str__(self):
         return self.store.primary_domain.host
 
+
 class Domain(models.Model):
-    host = models.CharField(max_length=255,blank=True, null=True,unique=True)
+    host = models.CharField(max_length=255, blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.host
 
+
 class StaffMember(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='staff_members')
-    store = models.ForeignKey('Store', on_delete=models.CASCADE, related_name='staff_members')
-    locale = models.CharField(max_length=50,blank=True, null=True)
-    account_access = models.CharField(max_length=100,blank=True, null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='staff_members')
+    store = models.ForeignKey(
+        'Store', on_delete=models.CASCADE, related_name='staff_members')
+    locale = models.CharField(max_length=50, blank=True, null=True)
+    account_access = models.CharField(max_length=100, blank=True, null=True)
     is_store_owner = models.BooleanField(default=False)
-    name = models.CharField(max_length=255,blank=True, null=True)
-    first_name = models.CharField(max_length=255,blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     permissions = models.JSONField(blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Staff Members"
-        unique_together = ['user','store']
+        unique_together = ['user', 'store']
 
     def __str__(self):
         return str(self.name)
 
+
 class Store(models.Model):
-    owner = models.OneToOneField(StaffMember,blank=True, null=True, on_delete=models.CASCADE,related_name='owned_store')
-    name = models.CharField(max_length=255,default='My Store')
+    owner = models.OneToOneField(
+        StaffMember, blank=True, null=True, on_delete=models.CASCADE, related_name='owned_store')
+    name = models.CharField(max_length=255, default='My Store')
     email = models.EmailField(blank=True, null=True)
-    default_domain = models.CharField(max_length=255,blank=True, null=True,unique=True)
-    primary_domain = models.OneToOneField(Domain, on_delete=models.CASCADE,blank=True, null=True)
+    default_domain = models.CharField(
+        max_length=255, blank=True, null=True, unique=True)
+    primary_domain = models.OneToOneField(
+        Domain, on_delete=models.CASCADE, blank=True, null=True)
     currency_code = models.CharField(max_length=5, blank=True, null=True)
     enabled_presentment_currencies = models.JSONField(default=list, blank=True)
+
     def save(self, *args, **kwargs):
         if not self.default_domain:
             self.default_domain = generate_unique_subdomain()
             if not self.primary_domain:
-                self.primary_domain = Domain.objects.create(host=self.default_domain)
+                self.primary_domain = Domain.objects.create(
+                    host=self.default_domain)
         if self.currency_code is None:
             self.currency_code = 'USD'
             self.enabled_presentment_currencies = ['USD']
