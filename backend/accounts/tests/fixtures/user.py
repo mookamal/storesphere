fc5797a5ff64625e568353297cfaa86407a11546
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-12-27 07:34:57 +0200

    add

--- backend/accounts/tests/fixtures/user.py
+++ backend/accounts/tests/fixtures/user.py
@@ -1,7 +1,12 @@
 import pytest
 from ...models import User
+from allauth.account.models import EmailAddress
 
 
 @pytest.fixture
 def user(db):
-    return User.objects.create_user(email='test@example.com', password='password')
+    user = User.objects.create_user(
+        email='test@example.com', password='password')
+    EmailAddress.objects.create(
+        user=user, email=user.email, verified=True, primary=True)
+    return user
