88c945234a593edc72700d460f27f10a9f950d8e
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2025-01-04 03:17:34 +0200

    add

--- backend/stores/signals.py
+++ backend/stores/signals.py
@@ -1,9 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Store, StoreAddress
+from .models import Store, StoreAddress, StaffMember
 
 
 @receiver(post_save, sender=Store)
 def set_default_store_address(sender, instance, created, **kwargs):
     if created:
         StoreAddress.objects.create(country="EG", store=instance)
+
+
+@receiver(post_save, sender=StaffMember)
+def assign_owner_to_store(sender, instance, created, **kwargs):
+    if created:
+        store = instance.store
+        if store and store.owner is None:
+            store.owner = instance
+            store.save()
