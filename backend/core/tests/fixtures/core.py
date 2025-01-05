635fe814ae5abfb514952809d3fc90a639f4b75b
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2025-01-05 03:56:28 +0200

    add

--- backend/core/tests/fixtures/core.py
+++ backend/core/tests/fixtures/core.py
@@ -3,5 +3,5 @@ from ...models import SEO
 
 
 @pytest.fixture
-def seo():
+def seo(db):
     return SEO.objects.create(title="seo title", description="seo description")
---
+++ backend/core/tests/test_seo.py
@@ -0,0 +1,4 @@
+
+def test_seo_fixture(seo):
+    assert seo.title == "seo title"
+    assert seo.description == "seo description"
