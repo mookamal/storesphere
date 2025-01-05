d39efcf848b6b7d82567d6429b4fc01a91d65969
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2025-01-05 04:16:44 +0200

    add

--- backend/product/tests/fixtures/product.py
+++ backend/product/tests/fixtures/product.py
@@ -1,6 +1,14 @@
 import pytest
+from ...models import Product, ProductVariant
+from decimal import Decimal
 
 
 @pytest.fixture
-def product():
-    pass
+def product(db, store, seo):
+    product = Product.objects.create(
+        title="Test product", store=store, seo=seo)
+    first_variant = ProductVariant.objects.create(
+        product=product, sku="123", price_amount=Decimal(10), stock=10)
+    product.first_variant = first_variant
+    product.save()
+    return product
