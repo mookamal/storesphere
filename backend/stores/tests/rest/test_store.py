aafbba4a1a87976d0c87964431ac3a9e94438e54
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2025-01-05 02:25:56 +0200

    add

--- backend/stores/tests/rest/test_store.py
+++ backend/stores/tests/rest/test_store.py
@@ -18,3 +18,26 @@ def test_store_detail_view(rest_api_client, user, store, staff_member):
     )
     assert response.status_code == 200
     assert response.data["name"] == "Test Store"
+
+
+def test_first_store_detail_view(rest_api_client, user, store, staff_member):
+    response = rest_api_client.get(
+        '/s/stores/first-store/',
+        SERVER_NAME="api.nour.com",
+        content_type="application/json"
+    )
+    assert response.status_code == 200
+
+
+def test_store_create_view(rest_api_client, user):
+    data = {
+        "name": "New Store",
+    }
+    response = rest_api_client.post(
+        path='/s/stores/create/',
+        data=data,
+        SERVER_NAME="api.nour.com",
+        format='json'
+    )
+    assert response.status_code == 201
+    assert response.data["name"] == "New Store"
