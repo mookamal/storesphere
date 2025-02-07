bbe39d827ac95b1b2e1be8b51f3bc673ff9955de
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2025-02-07 08:58:26 +0200

    add

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,15 +1,20 @@
+// src/lib/utils.ts
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs) {
+type GenericObject = { 
+  [key: string]: any 
+};
+
+export function cn(...inputs: any[]): string {
   return twMerge(clsx(inputs));
 }
 
-export function removeTypename(data) {
+export function removeTypename(data: any): any {
   if (Array.isArray(data)) {
     return data.map(removeTypename);
   } else if (data !== null && typeof data === "object") {
-    const cleaned = {};
+    const cleaned: GenericObject = {};
     Object.keys(data).forEach(key => {
       if (key !== '__typename') {
         cleaned[key] = removeTypename(data[key]);
