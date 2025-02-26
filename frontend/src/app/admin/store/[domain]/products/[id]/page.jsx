1e5464ec6f1e892a26e321617484185d230f6fa3
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2025-02-26 16:33:52 +0200

    Use hooks from codegen tool

--- frontend/src/app/admin/store/[domain]/products/[id]/page.jsx
+++ frontend/src/app/admin/store/[domain]/products/[id]/page.jsx
@@ -3,21 +3,10 @@
 import { Button } from "@/components/ui/button";
 import { IoReload } from "react-icons/io5";
 import { useForm, useFieldArray } from "react-hook-form";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useMemo  } from "react";
 import { useParams, notFound } from "next/navigation";
-import axios from "axios";
 import { debounce } from "lodash";
 import { toast } from "react-toastify";
-import {
-  GET_MEDIA_PRODUCT,
-  GET_PRODUCT_BY_ID,
-  GET_SETTINGS_GENERAL,
-} from "@/graphql/queries";
-import {
-  ADD_MEDIA_IMAGES_PRODUCT,
-  REMOVE_MEDIA_IMAGES_PRODUCT,
-  UPDATE_PRODUCT,
-} from "@/graphql/mutations";
 import LoadingElement from "@/components/LoadingElement";
 import PriceInput from "@/components/admin/product/common/PriceInput";
 import SeoInputs from "@/components/admin/product/common/SeoInputs";
@@ -36,42 +25,37 @@ import {
   useSettingsGeneralQuery,
   useGetMediaProductQuery,
   useRemoveMediaImagesProductMutation,
-  useUpdateProductMutation,
   ProductNode,
   ImageNode,
   CollectionNode,
   ProductInput,
   ProductProductStatusChoices,
   useAddMediaImagesProductMutation,
+  useGetProductByIdQuery,
+  useProductSaveUpdateMutation,
 } from "@/codegen/generated";
 export default function UpdateProduct() {
   const domain = useParams().domain;
-  const [loading, setLoading] = useState(false);
   const [isNotFound, setIsNotFound] = useState(false);
   const productId = useParams().id;
-  const [data, setData] = useState(null);
-  const [product, setProduct] = useState(null);
-  const [hasChanges, setHasChanges] = useState(false);
   const [selectedCollections, setSelectedCollections] = useState([]);
   const {
     register,
     handleSubmit,
     setValue,
+    reset,
     getValues,
     trigger,
     control,
-    formState: { errors },
+    formState: { errors,isDirty, dirtyFields },
     watch,
   } = useForm();
   const description = watch("description");
   const title = watch("title");
-  const status = watch("status");
   const handle = watch("handle");
-  const seoTitle = watch("seoTitle");
-  const seoDescription = watch("seoDescription");
-  const price = watch("price");
-  const compare = watch("compare");
-  const stock = watch("stock");
+  const seoTitle = watch("seo.title");
+  const price = watch("firstVariant.price");
+  const compare = watch("firstVariant.compareAtPrice");
   const options = watch("options");
   const { fields, append, remove } = useFieldArray({
     control,
@@ -120,49 +104,14 @@ export default function UpdateProduct() {
 
   const handleBlur = () => {
     if (!handle) {
-      setValue("handle", watchedTitle.replace(/\s+/g, "-").toLowerCase());
+      setValue("handle", title.replace(/\s+/g, "-").toLowerCase());
     }
     if (!seoTitle) {
-      setValue("seoTitle", watchedTitle);
+      setValue("seo.title", title);
     }
   };
 
-  useEffect(() => {
-    // Ensure all comparisons are safe and handle potential undefined values
-    const hasBasicChanges =
-      title !== product?.title ||
-      processDescription(description) !==
-        processDescription(product?.description) ||
-      status !== product?.status ||
-      handle !== product?.handle ||
-      seoTitle !== product?.seo?.title ||
-      seoDescription !== product?.seo?.description ||
-      safeParseNumber(price) !==
-        safeParseNumber(product?.firstVariant?.pricing?.amount) ||
-      safeParseNumber(compare) !==
-        safeParseNumber(product?.firstVariant?.compareAtPrice) ||
-      safeParseNumber(stock) !==
-        safeParseNumber(product?.firstVariant?.stock) ||
-      JSON.stringify(cleanOptionsData(options)) !==
-        JSON.stringify(cleanOptionsData(product?.options || [])) ||
-      JSON.stringify(cleanCollections(selectedCollections)) !==
-        JSON.stringify(cleanCollections(product?.collections || []));
 
-    setHasChanges(hasBasicChanges);
-  }, [
-    title,
-    description,
-    status,
-    handle,
-    seoTitle,
-    seoDescription,
-    price,
-    compare,
-    stock,
-    options,
-    selectedCollections,
-    product,
-  ]);
 
   const [addMediaImages, { loading: addImagesLoading }] =
     useAddMediaImagesProductMutation({
@@ -204,9 +153,7 @@ export default function UpdateProduct() {
     };
   }, [title, description]);
 
-  useEffect(() => {
-    getProductById();
-  }, []);
+
 
   const {
     data: mediaData,
@@ -235,110 +182,88 @@ export default function UpdateProduct() {
       console.error("Media fetch error:", error);
     },
   });
-
-  const getProductById = async () => {
-    try {
-      const response = await axios.post("/api/get-data", {
-        query: GET_PRODUCT_BY_ID,
-        variables: { id: productId },
-      });
-
-      if (response.data.error) {
-        throw new Error(response.data.error);
+// Get product data using the generated hook
+  const { data: product, loading: productLoading,refetch } = useGetProductByIdQuery({
+    variables: {
+      id: productId,
+      domain: domain,
+    },
+    skip: !productId || !domain,
+    onCompleted: (data) => {
+      // set product to form
+      if (data?.product) {
+        reset({
+          title: data.product.title || "",
+          description: data.product.description || "",
+          status: data.product.status || ProductProductStatusChoices.Draft,
+          handle: data.product.handle || "",
+          options: data.product.options || [],
+          seo:  {
+            title: data.product.seo?.title || "",
+            description: data.product.seo?.description || "",
+          },
+          firstVariant: {
+            price: data.product.firstVariant?.pricing?.amount || 0,
+            compareAtPrice: data.product.firstVariant?.compareAtPrice || 0,
+            stock: data.product.firstVariant?.stock || 0,
+          },
+        });
       }
-      if (response.data) {
-        setData(response.data || null);
-        setProduct(response.data.product || null);
-        setValue("title", response.data.product.title || "");
-        setValue("description", response.data.product.description || "");
-        setValue("status", response.data.product.status);
-        setValue("handle", response.data.product.handle || null);
-        setValue("seoTitle", response.data.product.seo.title || "");
-        setValue("seoDescription", response.data.product.seo.description || "");
-        setValue(
-          "price",
-          response.data.product.firstVariant.pricing.amount || 0.0
-        );
-        setValue(
-          "compare",
-          response.data.product.firstVariant.compareAtPrice || 0.0
-        );
-        setSelectedCollections(response.data.product.collections);
-        setValue("stock", response.data.product.firstVariant.stock || 0);
-        const optionsData = response.data.product.options || [];
-        if (optionsData.length > 0) {
-          const optionsWithEditingState = optionsData.map((option) => ({
-            ...option,
-            isEditing: false,
-          }));
-          setValue("options", optionsWithEditingState);
-        }
-      } else {
-        toast.error("Failed to fetch product details");
-        setIsNotFound(true);
-      }
-    } catch (error) {
-      toast.error("Failed to update product");
+    },
+    onError: (error) => {
+      toast.error("Failed to fetch product details");
       setIsNotFound(true);
+      console.error("Product fetch error:", error);
     }
-  };
+  });
 
+  // Update product mutation hook
+  const [updateProduct, { loading: updateLoading }] = useProductSaveUpdateMutation({
+    onCompleted: () => {
+      toast.success("Product updated successfully!");
+      reset(getValues());
+      refetch();
+    },
+    onError: (error) => {
+      toast.error("Failed to update product");
+      console.error("Update error:", error);
+    },
+  });
+
+  // Handle form submission
   const onSubmit = async (data) => {
-    setLoading(true);
-    const productData = {
-      title: data.title,
-      description: processDescription(data.description),
-      status: data.status,
-      handle: data.handle,
-      collectionIds: selectedCollections.map((c) => c.collectionId),
-      seo: {
-        title: data.seoTitle,
-        description: data.seoDescription,
-      },
-      firstVariant: {
-        price: safeParseNumber(data.price),
-        compareAtPrice: safeParseNumber(data.compare),
-        stock: safeParseNumber(data.stock),
-      },
-      options: cleanOptionsData(data.options),
-    };
+    if (typeof data.description === "object") {
+      data.description = JSON.stringify(data.description);
+    }
     const variables = {
       defaultDomain: domain,
-      product: productData,
+      product: data,
       id: productId,
     };
-    try {
-      const response = await axios.post("/api/set-data", {
-        query: UPDATE_PRODUCT,
-        variables: variables,
-      });
-
-      if (response.data.data.updateProduct.product.id) {
-        toast.success("Product updated successfully!");
-        getProductById();
-        setLoading(false);
-        setHasChanges(false);
-      }
-    } catch (error) {
-      console.error(error);
-      toast.error("Failed to update product");
-      setLoading(false);
-    }
+    updateProduct({ variables });
   };
 
+  const isLoading = useMemo(() => {
+    return removeImagesLoading || 
+           addImagesLoading || 
+           productLoading || 
+           updateLoading;
+  }, [removeImagesLoading, addImagesLoading, productLoading, updateLoading]);
+
+ 
   if (isNotFound) return notFound();
 
-  if (!data) {
-    return (
-      <div className="flex justify-center items-center h-full">
-        <IoReload className="mr-2 h-4 w-4 animate-spin" />
-      </div>
-    );
-  }
+  // if (!data) {
+  //   return (
+  //     <div className="flex justify-center items-center h-full">
+  //       <IoReload className="mr-2 h-4 w-4 animate-spin" />
+  //     </div>
+  //   );
+  // }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {loading && <LoadingElement />}
+      {isLoading && <LoadingElement />}
       <div className="p-5">
         <h1 className="h1">Update Product Details</h1>
         <div className="grid lg:grid-cols-2 gap-4 my-3">
@@ -348,12 +273,13 @@ export default function UpdateProduct() {
             handleBlur={handleBlur}
             description={description}
             setValue={setValue}
+            control={control}
           />
           {/* images input */}
           <MediaInputs
             selectedImages={mediaImages}
             setSelectedImages={setMediaImages}
-            externalLoading={loading}
+            externalLoading={isLoading}
             selectedRemoveImages={selectedRemoveImages}
             setSelectedRemoveImages={setSelectedRemoveImages}
             isEditMode={true}
@@ -396,9 +322,9 @@ export default function UpdateProduct() {
         size="lg"
         type="submit"
         className="fixed bottom-5 right-5 rounded-full shadow-md"
-        disabled={!hasChanges}
+        disabled={!isDirty}
       >
-        {loading && <IoReload className="mr-2 h-4 w-4 animate-spin" />}
+        {isLoading  && <IoReload className="mr-2 h-4 w-4 animate-spin" />}
         Update
       </Button>
     </form>
