990fee0efdf6f9f1d013c881d980fe9682ebdf03
Mohamed kamal <103385141+mookamal@users.noreply.github.com>
2025-07-27 13:39:41 +0300

    Update settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -333,8 +333,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mohamed.mohamed1996318@gmail.com'
-EMAIL_HOST_PASSWORD = 'dvyahitrmibcdoen'
+EMAIL_HOST_USER = 'EMAIL'
+EMAIL_HOST_PASSWORD = 'PASS'
 
 
 DEFAULT_DECIMAL_PLACES = 3
